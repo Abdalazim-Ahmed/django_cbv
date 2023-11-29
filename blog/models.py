0f33d18ff18be2117d8c1a5fcdf5682022721109
abdalazim <abdalazim@gmail.com>
2023-11-29 13:23:06 +0200

    inital

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.CharField(max_length=1000)
+    image = models.ImageField(upload_to='img', null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
