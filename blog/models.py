49943961fc691c8f89f0c84318c8ad9f2c1162c8
abdalazim <abdalazim@gmail.com>
2023-12-03 08:00:07 +0200

    add atter

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,9 @@ from django.utils import timezone
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     text = models.CharField(max_length=1000)
-    image = models.ImageField(upload_to='img', null=True, blank=True)
+    image = models.ImageField(upload_to='img/%Y/ %m/ %d')
     created_at = models.DateTimeField(default=timezone.now)
+    active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -7,8 +7,11 @@ from django.views.generic import ListView, DetailView
 
 class Blog_List(ListView):
     model = Blog
-
+    template_name = 'blog/blog.html'
+    context_object_name = 'blog'
+    ordering = ['-title']
+    queryset = Blog.objects.filter(active=True)
 
 
 class Blog_Detail(DetailView):
-    model = Blog    
+    model = Blog   
